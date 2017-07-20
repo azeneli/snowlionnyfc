4189dce0cf1b67a9b4dd28ac3304fb4260d1fd8c
Anri Zeneli <anri@seccosquared.com>
2017-07-20 12:07:38 -0400

    testing

--- js/contact_me.js
+++ js/contact_me.js
@@ -22,12 +22,14 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "./mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
+                    position: position,
+                    age: age,
                     message: message
                 },
                 cache: false,
